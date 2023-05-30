1009df261e43cca26db1192c2bb44581cf3efb8d
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-05-30 08:52:34 -0700

    shows the controlled form added create addRez to app

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,8 +1,61 @@
-import React from "react";
+import React, {useState } from "react";
 
-const Form = () => {
+const Form = (props) => {
+    const [name, setName] = useState('')
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
+    const [guests, setGuests] = useState(0)
+
+    const submitRez = event => {
+event.preventDefault()
+const newRez = {
+    id: Date.now(),
+    name,
+    date,
+    time,
+    guests
+}
+
+clearInputs()
+}
+const clearInputs = () => {
+    setDate('')
+    setName('')
+    setTime('')
+    setGuests(0)
+}
     return (
-        <div>Form</div>
+            <form>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                    />
+                    <input
+                    type="text"
+                    placeholder="Date (mm/dd)"
+                    name="date"
+                    value={date}
+                    onChange={event => setDate(event.target.value)}
+                    />
+                     <input
+                    type="text"
+                    placeholder="Time"
+                    name="time"
+                    value={time}
+                    onChange={event => setTime(event.target.value)}
+                    />
+                     <input
+                    type="number"
+                    placeholder="Number of Guests"
+                    name="guests"
+                    value={guests}
+                    onChange={event => setGuests(event.target.value)}
+                    />
+                    <button onClick={submitRez}></button>
+            </form>
     )
 }
 
